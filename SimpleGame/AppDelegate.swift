37b5a4041271e7ae148789e112c11cdec46ac0e2
Gordon Ellis <gellis12@gmail.com>
2014-11-29 09:43:46 -0800

    GameKit sample project

---
+++ SimpleGame/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SimpleGame
+//
+//  Created by Gordon Ellis on 2014-11-29.
+//  Copyright (c) 2014 Gordon Ellis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
---
+++ SimpleGameTests/SimpleGameTests.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleGameTests.swift
+//  SimpleGameTests
+//
+//  Created by Gordon Ellis on 2014-11-29.
+//  Copyright (c) 2014 Gordon Ellis. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class SimpleGameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
